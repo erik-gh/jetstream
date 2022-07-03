2afb9a70c28089418f33921f84d65954f7785bc2
erik-gh <eriktecse@gmail.com>
2022-07-02 20:42:03 -0500

    Se instalo npm install autoprefixer@10.4.5 --save-exact

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,12 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        @livewireStyles
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    @livewireStyles
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="{{ mix('js/app.js') }}" defer></script>
+{{--        @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
